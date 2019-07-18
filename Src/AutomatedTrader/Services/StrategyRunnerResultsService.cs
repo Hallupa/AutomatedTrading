39bcfe10035b7f60c1793d01fd8bf692959683d9
Oliver Wickenden <ocwickenden@hotmail.com>
2019-07-18 21:56:13 +0100

    Initial application version

---
+++ Src/AutomatedTrader/Services/StrategyRunnerResultsService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using TraderTools.Basics;
+
+namespace AutomatedTrader.Services
+{
+    [Export(typeof(StrategyRunnerResultsService))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class StrategyRunnerResultsService
+    {
+        private Subject<List<TradeDetails>> _testResultsUpdated = new Subject<List<TradeDetails>>();
+
+        public IObservable<List<TradeDetails>> TestResultsUpdated => _testResultsUpdated.AsObservable();
+
+        public void AddResult(List<TradeDetails> result)
+        {
+            lock (Results)
+            {
+                Results.AddRange(result);
+            }
+
+            _testResultsUpdated.OnNext(result);
+        }
+
+        public void Reset()
+        {
+            lock (Results)
+            {
+                Results.Clear();
+            }
+
+            _testResultsUpdated.OnNext(new List<TradeDetails>());
+        }
+
+        public List<TradeDetails> Results { get; } = new List<TradeDetails>();
+    }
+}
---
+++ Src/AutomatedTrader/Views/StrategyRunResultsView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using AutomatedTrader.ViewModels;
+
+namespace AutomatedTrader.Views
+{
+    /// <summary>
+    /// Interaction logic for StrategyRunResultsView.xaml
+    /// </summary>
+    public partial class StrategyRunResultsView : Page
+    {
+        public StrategyRunResultsView()
+        {
+            InitializeComponent();
+
+            DataContext = new StrategyRunResultsViewModel();
+        }
+    }
+}
